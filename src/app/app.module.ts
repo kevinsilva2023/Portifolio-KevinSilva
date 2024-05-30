bd38047c945f48a28f48be7407d8d1932f730622
Kevin Silva <149127319+kevinsilva2023@users.noreply.github.com>
2024-05-29 21:27:25 -0300

    update mennus phone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Rotas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './app/home/home.component';
+import { MenuLateralComponent } from './app/home/menu-lateral/menu-lateral.component';
+import { ConteudoPrincipalComponent } from './app/home/conteudo-principal/conteudo-principal.component';
+import { SideBarComponent } from './app/home/conteudo-principal/side-bar/side-bar.component';
+import { SobreMimComponent } from './app/home/conteudo-principal/sobre-mim/sobre-mim.component';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SideBarComponent,
+    MenuLateralComponent,
+    ConteudoPrincipalComponent,
+    SobreMimComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app/home/conteudo-principal/conteudo-principal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conteudo-principal',
+  templateUrl: './conteudo-principal.component.html',
+  styleUrls: ['./conteudo-principal.component.scss']
+})
+export class ConteudoPrincipalComponent {
+
+}
