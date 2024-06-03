44bc176c73beb4e4fda365a27be290de180ece24
Kevin Silva <149127319+kevinsilva2023@users.noreply.github.com>
2024-06-03 19:32:45 -0300

    refatorando portifolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 // Componentes
 import { AppComponent } from './app.component';
-import { HomeComponent } from './app/home/home.component';
-import { SideBarComponent } from './app/home/side-bar/side-bar.component';
-import { MenuLateralComponent } from './app/home/menu-perfil/menu-perfil.component';
-import { ConteudoPrincipalComponent } from './app/home/conteudo-principal/conteudo-principal.component';
-import { SobreMimComponent } from './app/home/conteudo-principal/sobre-mim/sobre-mim.component';
+
 
 // Angular Material
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    SideBarComponent,
-    MenuLateralComponent,
-    ConteudoPrincipalComponent,
-    SobreMimComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
